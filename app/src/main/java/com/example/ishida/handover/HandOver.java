768dc6fbdec9a567122b1e7e9d3c5a39be490ec1
Shoji Ishida <shoji.ishida@gmail.com>
2015-02-16 18:31:24 +0900

    Initial commit

--- app/src/main/java/com/example/ishida/handover/HandOver.java
+++ app/src/main/java/com/example/ishida/handover/HandOver.java
@@ -1,7 +1,11 @@
 package com.example.ishida.handover;
 
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.RemoteException;
 
 import java.util.HashMap;
@@ -18,6 +22,7 @@ public class HandOver {
     private Activity activity;
     private Map<String, Object> dictionary;
     private HandOverCallback callback = null;
+    private IHandOverService handOverService;
 
     private HandOver(Activity activity) {
         this.activity = activity;
@@ -29,17 +34,26 @@ public class HandOver {
     }
 
 
-    public void registerCallback(HandOverCallback calllback) {
-        this.callback = calllback;
+    public void registerCallback(HandOverCallback callback) {
+        this.callback = callback;
     }
 
     private IHandOverCallback handOverCallback = new IHandOverCallback.Stub() {
         @Override
         public void handleHandOver() throws RemoteException {
-
         }
     };
 
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            handOverService = IHandOverService.Stub.asInterface(service);
+        }
 
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            handOverService = null;
+        }
+    };
 
 }
